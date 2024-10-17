032c5f270854cff35b9eb7d18b87656b1e9e954c
IbrahimAwd-coder <adhamawd92@gmail.com>
2024-10-17 12:23:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+ 
+// function add(){
+// let name=$('#name').val();
+
+// if(name==''){
+// // document.getElementById('name').classList.add('error');
+// $('#name').addClass('error');
+// }else{
+//     // document.getElementById('name').classList.remove('error');
+//     $('#name').removeClass('error');
+//     alert(name);
+// }
+
+
+// }  
+    
+// $('#cl').dblclick(function(){
+// $('#div').show(function(){
+//     $(this).slideUp();
+// });
+   
+// });
+
+// $('#cle').click(function(){
+
+// $('#nn').slideToggle();
+
+// });
+
+// $(window).scroll(function(){
+// let current=$(window).scrollTop(); 
+// console.log(current);
+// });
+  
+// $('#dd').hover(function(){
+// $('#ddd').show(function(){
+// $('#ddd').slideToggle(1000);
+
+
+// });
+
+// // });
+// function add(){
+// let username=$('.row').html();
+// alert(username);
+// }
+// function plus(){
+// let html=`
+//   <h1 id="name">hema</h1>
+// <input type="text" name="" class="form-control" id="user">
+// <button class="btn btn-success" onclick="add()">add</button>
+// <button class="btn btn-danger" onclick="remove()">remove</button>
+
+
+// `;
+// $('#rst').append(html);
+  
+// }
+
+// function remove(){
+// // let html= `
+// //     <h1 id="name">hema</h1>
+// //     <input type="text" name="" class="form-control" id="user">
+// //     <button class="btn btn-success" onclick="add()">add</button>
+// //     <button class="btn btn-danger" onclick="remove()">remove</button>
+    
+    
+// //     `;
+//     $('#rst').remove();
+    
+
+
+// }
+
+// $('.hf ul li ').click(function (){
+
+// $(this).children().addClass('active');
+
+// $(this).siblings().children().removeClass('active');
+// $(this).siblings().children().toggle();
+// })
+
+
+// var nice = $("body").niceScroll({
+//         preservenativescrolling: false,
+//         cursorwidth: '8px',
+//         cursorborder: 'none',
+//         cursorborderradius:'0px',
+//         cursorcolor:"#39CCDB",
+//         autohidemode: false, 
+//         background:"#999999"
+//      });
+
+
+
+// $(document).ready(function(){
+// $('.kl').hide();
+
+// // });
+// $('.count').each(function () {
+//     var $this = $(this);
+//     jQuery({ Counter: 0 }).animate({ Counter: $this.attr('data-stop') }, {
+//       duration: 1000,
+//       easing: 'swing',
+//       step: function (now) {
+//         $this.text(Math.ceil(now));
+//       }
+//     });
+//   });
+
+
+// $(window).scroll(function(){
+
+// $('.nn').body.style="";
+
+// // });
+
+// $(  function() {
+// //   $('.datepicker').datepicker();
+// // });
+// $('.sg1').click(function(){
+//   $('.datepicker').datepicker();
+// });
+
+
+
+
+// $(  function() {
+//   $('.datepicker').datepicker();
+// });
+
+
+$(function(){
+  $('#datepicker').datepicker();
+});
+
